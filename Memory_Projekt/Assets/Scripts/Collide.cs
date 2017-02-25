8a13f88978783b2b81cae79c21d6128c90e2b3ff
PRGAnna <mdina@gmx.net>
2017-02-25 10:36:07 +0100

    Neue Cubes und Collide Script hinzugefügt

---
+++ Memory_Projekt/Assets/Scripts/Collide.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collide : MonoBehaviour {
+
+	
+	void OnCollisionEnter (Collision col) {
+		if(col.gameObject.name == "trigger")
+        { Destroy(col.gameObject);
+        }
+	}
+}
