5627da5bf2d367c66078e9e650f3acbf2527bfcf
PRGAnna <mdina@gmx.net>
2017-03-02 18:14:43 +0100

    Collide Script geändert

--- Memory_Projekt/Assets/Scripts/Collide.cs
+++ Memory_Projekt/Assets/Scripts/Collide.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Collide : MonoBehaviour {
 
-	
-	void OnCollisionEnter (Collision col) {
+    public GameObject prefab;
+
+    void OnCollisionEnter (Collision col) {
 		if(col.gameObject.name == "trigger")
-        { Destroy(col.gameObject);
+        {
+            Instantiate(prefab, new Vector3(2.0f, 0, 0), Quaternion.identity);
         }
 	}
 }
