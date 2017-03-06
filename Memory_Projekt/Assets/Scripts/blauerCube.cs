c2c9ddd8bce18b3b03e1df7e32d3646aab141783
PRGAnna <mdina@gmx.net>
2017-03-06 22:25:41 +0100

    RadialMenu gelöscht, Cube Prefabs erstellt

---
+++ Memory_Projekt/Assets/Scripts/blauerCube.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blauerCube : MonoBehaviour {
+
+    public GameObject prefab;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "SaeuleBlau")
+        {
+            Instantiate(prefab, new Vector3(-7.2f, 1, 3.1f), Quaternion.identity);
+        }
+    }
+}
