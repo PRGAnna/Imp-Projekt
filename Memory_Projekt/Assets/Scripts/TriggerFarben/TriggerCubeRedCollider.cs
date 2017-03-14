20df4033a363270e0ee435d224f9f392efb47078
PRGAnna <mdina@gmx.net>
2017-03-14 20:05:28 +0100

    andere Skybox

--- Memory_Projekt/Assets/Scripts/TriggerFarben/TriggerCubeRedCollider.cs
+++ Memory_Projekt/Assets/Scripts/TriggerFarben/TriggerCubeRedCollider.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class TriggerCubeRedCollider : MonoBehaviour {
 
     public GameObject prefab;
-	public Color32 color = new Color (244, 18, 18, 255);
+
+
 
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.name == "triggerRed")
         {
             Instantiate(prefab, new Vector3(-64.1f, 0, 7.7f), Quaternion.identity);
-			prefab.GetComponent<Renderer>().material.color = color;
+
         }
     }
 }
