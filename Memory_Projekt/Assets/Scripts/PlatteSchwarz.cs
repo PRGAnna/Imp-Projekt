d857bd5a85fa435fb26392be17bb65c3f6b623a0
PRGAnna <mdina@gmx.net>
2017-03-15 12:27:22 +0100

    letztes Raetsel fertig

---
+++ Memory_Projekt/Assets/Scripts/PlatteSchwarz.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatteSchwarz : MonoBehaviour {
+
+	public GameObject prefab;
+
+
+	void OnCollisionEnter (Collision col) {
+		if(col.gameObject.name == "platteSchwarz")
+		{
+			Application.LoadLevel ("Szene");
+		}
+	}
+}
---
+++ Memory_Projekt/Assets/triggerLetztes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerLetztes : MonoBehaviour {
+
+	//public GameObject triggerRaetsel;
+	public GameObject prefab;
+
+
+	void OnCollisionEnter (Collision col) {
+		if(col.gameObject.name == "TriggerLetztesRaetsel")
+		{
+			Instantiate(prefab, new Vector3(-128.31f, 4, 187), Quaternion.identity);
+		}
+	}
+}
